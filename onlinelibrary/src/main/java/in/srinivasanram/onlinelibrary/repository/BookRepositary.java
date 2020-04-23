df3ef8f5f9f82bdc88ace3a17c0132ce581140b9
Srinivasan Ramesh <srinivasan21121993@gmail.com>
2020-04-23 20:56:37 +0530

    Created Angular Project and Completed feature 1

--- onlinelibrary/src/main/java/in/srinivasanram/onlinelibrary/repository/BookRepositary.java
+++ onlinelibrary/src/main/java/in/srinivasanram/onlinelibrary/repository/BookRepositary.java
@@ -1,9 +1,11 @@
 package in.srinivasanram.onlinelibrary.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import in.srinivasanram.onlinelibrary.entity.Book;
 
+@CrossOrigin("http://localhost:4200")
 public interface BookRepositary extends JpaRepository<Book, Long>{
 
 }
