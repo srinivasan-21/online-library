d30b72a3227ab3b2ae4da0c8dfd568f45b539fe4
Srinivasan Ramesh <srinivasan21121993@gmail.com>
2020-04-26 21:52:06 +0530

    Completed Feature 3 search by category

--- onlinelibrary/src/main/java/in/srinivasanram/onlinelibrary/repository/BookRepositary.java
+++ onlinelibrary/src/main/java/in/srinivasanram/onlinelibrary/repository/BookRepositary.java
@@ -1,11 +1,18 @@
 package in.srinivasanram.onlinelibrary.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import in.srinivasanram.onlinelibrary.entity.Book;
 
 @CrossOrigin("http://localhost:4200")
 public interface BookRepositary extends JpaRepository<Book, Long>{
+	
+	@RestResource(path = "categoryid")
+	Page<Book> findByCategoryId(@Param("id") Long id, Pageable pageable);
 
 }
